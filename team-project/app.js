2c0a357d3414f87e9afe29b682961a84b1c491c4
bagdad1122 <bohdanchermak1@gmail.com>
2026-06-19 19:04:40 +0300

    Update app.js

--- team-project/app.js
+++ team-project/app.js
@@ -1,1 +1,25 @@
+let evData = [];
 
+async function loadData() {
+    try {
+        const response = await fetch('data.json');
+
+        if (!response.ok) {
+            throw new Error(`Помилка HTTP: ${response.status}`);
+        }
+        
+        const data = await response.json();
+
+        evData = data.categories;
+        
+        console.log("Дані успішно завантажено:", evData);
+        
+    } catch (error) {
+        console.error("Помилка при завантаженні даних:", error);
+        document.getElementById('main-content').innerHTML = 
+            `<div class="alert alert-danger">Не вдалося завантажити дані. Перевірте консоль.</div>`;
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    loadData();
+});
